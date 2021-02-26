aa8470f0bf878bffbd26ad4461dfc0dc26f6361b
AndrigoPiai <62120994+AndrigoPiai@users.noreply.github.com>
2021-02-25 23:02:49 -0300

    Update lab1_main.c

--- lab1/src/lab1_main.c
+++ lab1/src/lab1_main.c
@@ -1,17 +1,3 @@
-//#include <stdio.h>
-//#include <stdint.h>
-//#include <stdbool.h>
-//// includes da biblioteca driverlib
-//#include "inc/hw_memmap.h"
-//#include "driverlib/gpio.h"
-//#include "driverlib/uart.h"
-//#include "driverlib/sysctl.h"
-//#include "driverlib/systick.h"
-//#include "driverlib/pin_map.h"
-//#include "utils/uartstdio.h"
-//
-//#include "system_TM4C1294.h"
-
 #include <stdint.h>
 #include <stdbool.h>
 // includes da biblioteca driverlib
@@ -19,7 +5,6 @@
 #include "driverlib/gpio.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/systick.h"
-
 
 #include "system_TM4C1294.h"
 
@@ -34,21 +19,21 @@ void main(void){
   SysTickPeriodSet(12000000); // f = 1Hz para clock = 24MHz
   
   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION); // Habilita GPIO N (LED D1 = PN1, LED D2 = PN0)
-  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION)); // Aguarda final da habilitaÁ„o
+  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION)); // Aguarda final da habilita√ß√£o
   
-  GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0 | GPIO_PIN_1); // LEDs D1 e D2 como saÌda
+  GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0 | GPIO_PIN_1); // LEDs D1 e D2 como sa√≠da
   GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0 | GPIO_PIN_1, 0); // LEDs D1 e D2 apagados
   GPIOPadConfigSet(GPIO_PORTN_BASE, GPIO_PIN_0 | GPIO_PIN_1, GPIO_STRENGTH_12MA, GPIO_PIN_TYPE_STD);
 
   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); // Habilita GPIO F (LED D3 = PF4, LED D4 = PF0)
-  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)); // Aguarda final da habilitaÁ„o
+  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)); // Aguarda final da habilita√ß√£o
     
-  GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_4); // LEDs D3 e D4 como saÌda
+  GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_4); // LEDs D3 e D4 como sa√≠da
   GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_4, 0); // LEDs D3 e D4 apagados
   GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_4, GPIO_STRENGTH_12MA, GPIO_PIN_TYPE_STD);
 
   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOJ); // Habilita GPIO J (push-button SW1 = PJ0, push-button SW2 = PJ1)
-  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOJ)); // Aguarda final da habilitaÁ„o
+  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOJ)); // Aguarda final da habilita√ß√£o
     
   GPIOPinTypeGPIOInput(GPIO_PORTJ_BASE, GPIO_PIN_0 | GPIO_PIN_1); // push-buttons SW1 e SW2 como entrada
   GPIOPadConfigSet(GPIO_PORTJ_BASE, GPIO_PIN_0 | GPIO_PIN_1, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
@@ -60,7 +45,7 @@ void main(void){
     unsigned long j=0;
       
     for(j=0; j<= (SystemCoreClock/18);j++){
-        ; //o '18' foi tentativa e erro para +-1s -> o certo È pesquisar quantos ciclos de clock leva pra realizar uma interaÁ„o do for
+        ; //o '18' foi tentativa e erro para +-1s -> o certo √© pesquisar quantos ciclos de clock leva pra realizar uma intera√ß√£o do for
     }
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, GPIO_PIN_0); // Acende LED D4
    
